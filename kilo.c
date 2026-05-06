8b7263ce3882f5d35a036ea65da6852cd8ed068b
Tomix00 <tomi.code@hotmail.com>
2026-05-06 22:34:51 +0000

    Limited scrolling to the right

--- kilo.c
+++ kilo.c
@@ -374,6 +374,9 @@ void editorRefreshScreen(){
 
 void editorMoveCursor(int key){
     // moves the cursor
+
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
     switch (key){
         case ARROW_LEFT:
             if (E.cx != 0){
@@ -381,7 +384,9 @@ void editorMoveCursor(int key){
             }
             break;
         case ARROW_RIGHT:
-            E.cx++;
+            if (row && E.cx < row->size){
+                E.cx++;
+            }
             break;
         case ARROW_UP:
             if (E.cy != 0){
